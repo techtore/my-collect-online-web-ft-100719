d836cd4dd0f570f4c3ffac9e247fc53bb91ea172
Victoria Hale <vic.hale74@gmail.com>
2019-10-10 17:36:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,8 @@ def my_collect(array)
   i = 0 
   new_array = []
   while i < array.length 
-  yield array[i]
-  
-  new_array << array
-  
+  yield  new_array << array[i]
+
   i += 1
   end
   new_array
